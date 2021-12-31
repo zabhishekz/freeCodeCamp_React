b497c7365a9a79364857eeb34dde4a83562562c7
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-31 14:30:32 +0530

    JSX Rules - 2

--- src/index.js
+++ src/index.js
@@ -1,62 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// JSX Rules
-// return single element
-// div/section/article or fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
-//example -1
-// function Greeting() {
-//   return (
-//     <div>
-//       <div>
-//         <h3>hello world</h3>
-//         <ul>
-//           <li>
-//             <a href="#">hello world</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <div></div>
-//     </div>
-//   );
-// }
-
-//example -2: using React.fragment
-// function Greeting() {
-//   return (
-//     <React.Fragment>
-//       <div>
-//         <h3>hello world</h3>
-//         <ul>
-//           <li>
-//             <a href="#">hello world</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <div></div>
-//     </React.Fragment>
-//   );
-// }
-
-//example -3: using React.fragment (shortcut)
 function Greeting() {
   return (
-    <>
-      <div>
-        <h3>hello world</h3>
-        <ul>
-          <li>
-            <a href="#">hello world</a>
-          </li>
-        </ul>
-      </div>
-      <div></div>
-    </>
+    <div className="">
+      <h3>hello world</h3>
+      <ul>
+        <li>
+          <a href="#">hello world</a>
+        </li>
+        <img src="" alt="" />
+      </ul>
+    </div>
   );
 }
 
