0a90d90be258f6110bb868d613f6cca2d74cf300
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-31 14:25:17 +0530

    JSX Rules - 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//stateless functional component
-//always return JSX
+// JSX Rules
+// return single element
+// div/section/article or fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
+//example -1
 // function Greeting() {
-//   return <h4>hello world</h4>;
+//   return (
+//     <div>
+//       <div>
+//         <h3>hello world</h3>
+//         <ul>
+//           <li>
+//             <a href="#">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <div></div>
+//     </div>
+//   );
 // }
 
-const Greeting = () => {
-  return React.createElement("h1", {}, "hello world");
-};
+//example -2: using React.fragment
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h3>hello world</h3>
+//         <ul>
+//           <li>
+//             <a href="#">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <div></div>
+//     </React.Fragment>
+//   );
+// }
+
+//example -3: using React.fragment (shortcut)
+function Greeting() {
+  return (
+    <>
+      <div>
+        <h3>hello world</h3>
+        <ul>
+          <li>
+            <a href="#">hello world</a>
+          </li>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
+}
 
 ReactDOM.render(<Greeting />, document.getElementById("root"));
