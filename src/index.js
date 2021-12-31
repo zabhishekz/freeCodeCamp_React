ca1798b0f78ebe15691662d4795f31df088c0ac0
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-31 13:39:15 +0530

    First Component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//In order for react to know it is going to be a component, function should be setup with Capital initial letter
+function Greeting() {
+  return <h4>This is Abhishek and this is my first component</h4>;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Greeting />, document.getElementById("root"));
