60cea4ed4157f5f0f3587156500fac2de8f6215b
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-01 13:16:13 +0530

    Props Destructuring- example 1

--- src/index.js
+++ src/index.js
@@ -34,12 +34,12 @@ function BookList() {
 }
 
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
