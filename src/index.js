d997053f34403f781a0380b39944341bb27be33f
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-01 15:42:16 +0530

    Event Basics

--- src/index.js
+++ src/index.js
@@ -34,13 +34,32 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}> {title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
