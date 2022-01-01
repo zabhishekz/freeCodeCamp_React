f16e9d7163ae8c2b9cfe44484323727e8950a1a9
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-01 18:37:27 +0530

    Import And Export Statements

--- src/index.js
+++ src/index.js
@@ -2,27 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX258_BO1,204,203,200_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth",
-  },
-  {
-    id: 2,
-    img: "https://images-eu.ssl-images-amazon.com/images/I/510g8NLbpNL._SX198_BO1,204,203,200_QL40_FMwebp_.jpg",
-    title: "Our Class is a Family",
-    author: "Shannon Olsen",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/41EzNnr4YUL._SY264_BO1,204,203,200_QL40_ML2_.jpg",
-    title: "The Vanishing Half",
-    author: "Brit Bennett",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -33,35 +14,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} />
-      <h1 onClick={() => console.log(title)}> {title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
